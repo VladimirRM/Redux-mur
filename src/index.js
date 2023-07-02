5ba9e3919bb1e793a28d20e64ab75b91e43d9ac1
Vladimir RM <shvdrummer@gmail.com>
2023-07-02 17:24:24 +0300

    Add reeucer and initialState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { createStore } from 'redux'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const defaultState = {
+  cash: 0,
+}
+
+const reducer = (state = defaultState,action)=>{
+  switch(action,type){
+        case "ADD_CASH":
+        
+        
+        case "GET_CASH":
+    default: return state
+  }
+}
+
+const store = createStore()
+
+
+
+const index = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default index
