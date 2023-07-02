44a09cfa5fcae060b182d6126278c4c172195bde
Vladimir RM <shvdrummer@gmail.com>
2023-07-02 17:10:41 +0300

    Update files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import './App.css';
-import store from './redux/store';
 
-import Likes from './Likes'
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="wrap">
-        <div className="card">
-          <div className="card-image">
-            <img src="./sea.jpg" alt="surfing"/>
-            <Likes store={store}/>
-          </div>
-        </div>
-      </div>
+   
     </div>
   );
 }
