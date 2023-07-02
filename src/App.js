3122bacd61427d3ff1091b4b5d4ca3a9a6612b7d
Vladimir RM <shvdrummer@gmail.com>
2023-07-02 10:17:59 +0300

    Add main markUp for the App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-   
+      <div className="wrap">
+        <div className="card">
+          <div className="card-image">
+            <img src="./sea.jpg" alt="surfing"/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
