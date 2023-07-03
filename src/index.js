786b21177b5d8806048402c80ed8c70000d4a0d6
Vladimir RM <shvdrummer@gmail.com>
2023-07-03 12:17:31 +0300

    Import  and  add Provider

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 const initialState = {
   cash:0,
 }
 
 
-const reducer = (state,action)=>{
-      switch(action,type){
-        case :{
+const reducer = (state = initialState,action)=>{
+      switch(action.type){
+        case 'ADD_CASH' :
+           return {...state, cash: state.cash + action.payload}
+        case 'GET_CASH' :
            
-        }
+           return {...state, cash: state.cash - action.payload}
 
+           
 
         default: return state
       }
@@ -26,9 +30,11 @@ const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
 );
 
 
