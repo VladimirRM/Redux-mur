a14a611c87332d75144ce9133354786216ef86de
Vladimir RM <shvdrummer@gmail.com>
2023-07-03 08:55:06 +0300

    Add dispatch customers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 
 import './App.css';
-  import {useDispatch,useSelector} from 'react-redux'
+import {useSelector,useDispatch} from 'react-redux'
 
 
 function App() {
 
+  const dispatch = useDispatch()
+
+  const cash = useSelector((state)=> state.cash.cash)
+  const customers = useSelector((state)=> state.customers.customers)
+
+  const addCash = (cash)=>{
+       dispatch({type:"ADD_CASH", payload: cash})
+  }
+
+  const getCash = ( cash)=>{
+    dispatch({type:"GET_CASH",payload: cash})
+  }
+
 
 
   return (
-    <div  style={{ fontSize: '100px'}}className={'app'}>
-      <div>{cash}</div>
-
+    <div  style={{ fontSize: '100px',justifyContent:'center' ,textAlign:'center'}}className={'app'}>
+      <div style={{textAlign:'center',
+        
+    }}>{cash}</div>
+      <button onClick={()=>addCash(Number(prompt()))}>ADD</button>
+      <button onClick={()=>getCash(Number(prompt()))}>GET</button>
+  
 
     </div>
   );
