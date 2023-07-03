bf13c40b8901e1ba02a877f65ea05569140cb82d
Vladimir RM <shvdrummer@gmail.com>
2023-07-03 20:12:57 +0300

    update code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 
 import './App.css';
-import { useDispatch,useSelector } from 'react-redux';
 
 function App() {
-    const dispatch = useDispatch()
-    const cash = useSelector((state)=> state.cash)
-
-
-    const addCash = ( cash) =>{
-        dispatch({type:"ADD_CASH",payload: cash})
-    }
-
-    const getCash = (cash) =>{
-        dispatch({type: "GET_CASH",payload: cash})
-    }
   return (
     <div className="App">
-        {cash}
-        <button onClick={()=>addCash(Number(prompt()))}>ADD CASH</button>
-        <button onClick={()=>getCash(Number(prompt()))}>GET CASH</button>
+   
     </div>
   );
 }
