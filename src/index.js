d97320a71304d1b9856b3f4402a74e68a0751080
Vladimir RM <shvdrummer@gmail.com>
2023-07-03 12:10:21 +0300

    Add initialState cash 0

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+
+const initialState = {
+  cash:0,
+}
+
+
+const reducer = (state,action)=>{
+      switch(action,type){
+        case :{
+           
+        }
+
+
+        default: return state
+      }
+}
+
+
+const store = createStore(reducer)
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
