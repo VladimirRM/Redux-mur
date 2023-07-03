b580d598dc9173dd0c8c6c9f0f7f5c2f05e3a50b
Vladimir RM <shvdrummer@gmail.com>
2023-07-03 12:24:49 +0300

    Add buttons

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@
 import './App.css';
 import './index.css'
 
+import { useDispatch,useSelector } from 'react-redux';
+
 function App() {
+
+    const dispatch = useDispatch()
+
+    const cash = useSelector((state)=>state.cash)
   return (
-    <div className="App">
+    <div  style={{display: 'flex'}}className="App">
+              {cash}
+           <button>Add CASH</button>
+           <button>Get CASH</button>
    
     </div>
   );
