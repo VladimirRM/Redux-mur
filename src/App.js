6a1509af8be114aa021faf4dfffe9a22531ccb15
Vladimir RM <shvdrummer@gmail.com>
2023-07-03 14:49:18 +0300

    Add new reducer

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 
 import './App.css';
-import './index.css'
-
 import { useDispatch,useSelector } from 'react-redux';
 
-
-
-
-
 function App() {
-
-
-
     const dispatch = useDispatch()
+    const cash = useSelector((state)=> state.cash)
 
-    const cash = useSelector((state)=>state.cash)
 
-    const addCash = (cash) =>{
-        dispatch({type:'ADD_CASH',payload:cash})
+    const addCash = ( cash) =>{
+        dispatch({type:"ADD_CASH",payload: cash})
     }
-    
+
     const getCash = (cash) =>{
-        dispatch({type:'GET_CASH',payload:cash})
-    
+        dispatch({type: "GET_CASH",payload: cash})
     }
   return (
-    <div  style={{ textAlign:'center' ,marginTop: '200px'}}className="App">
-              {cash}
-           <button onClick={()=>dispatch(addCash(Number(prompt()))) }>Add CASH</button>
-           <button onClick={()=>getCash()}>Get CASH</button>
-   
+    <div className="App">
+        {cash}
+        <button onClick={()=>addCash(Number(prompt()))}>ADD CASH</button>
+        <button onClick={()=>getCash(Number(prompt()))}>GET CASH</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,36 +5,35 @@ import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-const initialState = {
-  cash:0,
-}
 
+const initialState = {
+  cash: 0,
+}
 
 const reducer = (state = initialState,action)=>{
-      switch(action.type){
-        case 'ADD_CASH' :
-           return {...state, cash: state.cash + action.payload}
-        case 'GET_CASH' :
-           
-           return {...state, cash: state.cash - action.payload}
-
-           
-
-        default: return state
-      }
+        switch(action.type){
+          case 'ADD_CASH':{
+            return{...state,cash: state.cash + action.payload}
+          }
+          case "GET_CASH":{
+            return{...state,cash: state.cash - action.payload}
+          }
+          default: return state
+        }
 }
 
 
-const store = createStore(reducer)
 
 
+  const store = createStore(reducer)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
 
-    <App />
+<Provider store={store}>
+<App />
 
-  </Provider>
+</Provider>
+
 );
 
 
