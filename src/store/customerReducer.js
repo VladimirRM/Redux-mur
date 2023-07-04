e2c624dd77b61fc162fc82908cd7ad9edb907616
Vladimir RM <shvdrummer@gmail.com>
2023-07-04 10:01:24 +0300

    Add Provider for App

---
+++ src/store/customerReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+    customers: [],
+  }
+  
+   export const customerReducer = ( state = initialState,action)=>{
+         switch(action.type){
+          case 'ADD_CUSTOMERS':
+          return{...state,customers:[...state.customers,action.payload]}
+  
+          case 'REMOVE_CUSTOMERS':
+          return{...state,customers:state.customers.filter((customer)=> customer.id !==action.payload)}
+  
+          
+  
+          default: return state
+         }
+  }
