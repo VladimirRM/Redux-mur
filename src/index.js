a7580dc5cbf8bfa6b0d6583ea497525a9768c281
Vladimir RM <shvdrummer@gmail.com>
2023-07-06 17:57:34 +0300

    Create func reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const reducer  = (state,action)=>{
+   switch (action.type) {
+      case ADD_CASH:
+         return {...state,cash: state.cash + action.payload}
+
+         default: return state
+   }
+}
+
 const store = createStore(reducer)
 
 import { createStore } from 'redux';
