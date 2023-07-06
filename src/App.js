681a35e8f00abb1bafb28f096adf1d9087a19fed
Vladimir RM <shvdrummer@gmail.com>
2023-07-06 18:04:10 +0300

    Add func addCash

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { useDispatch,useSelector } from 'react-redux';
 function App() {
     const dispatch = useDispatch()
     const cash = useSelector((state)=> state.cash)
+
+    const addCash = (cash)=> {
+       dispatch({type:"ADD_CASH", payload:cash})
+    }
   return (
     <div className="App">
         {cash}
