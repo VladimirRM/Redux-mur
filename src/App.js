22d36d2f020703ef1e934fa7bb0d2180e3a67bd9
Vladimir RM <shvdrummer@gmail.com>
2023-07-07 08:15:49 +0300

    Add obj customer for function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import { useDispatch,useSelector } from 'react-redux';
+import { addCustomerAction } from './store/customerReducer';
 
 function App() {
     const dispatch = useDispatch()
@@ -12,11 +13,20 @@ function App() {
     const getCash = (cash)=> {
        dispatch({type:"GET_CASH", payload:cash})
     }
+
+    const addCustomers =( name)=>{
+        const customer = {
+           name,
+           id:Date.now()
+        }
+        dispatch(addCustomerAction(customer))
+    }
   return (
     <div className="App">
         {cash}
         <button onClick={()=>addCash(Number(prompt()))}>Add</button>
         <button onClick={()=>getCash(Number(prompt()))}>Get</button>
+        <button onClick={()=>addCustomers(prompt())}>Add customers</button>
     </div>
   );
 }
--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -2,14 +2,20 @@ const initialState = {
   customers:[],
 }
 
+const ADD_CUSTOMERS = 'ADD_CUSTOMERS'
+const GET_CUSTOMERS = 'GET_CUSTOMERS'
+
  export const customerReducer  = (state = initialState,action)=>{
   switch (action.type) {
-     case "ADD_CASH":
+     case ADD_CUSTOMERS:
         return {...state,cash: state.cash + action.payload}
-     case "GET_CASH":
+     case GET_CUSTOMERS:
         return {...state,cash: state.cash - action.payload}
 
         default: return state
   }
 }
+
+export const addCustomerAction = (payload)=>({type: ADD_CUSTOMERS,payload})
+export const getCustomerAction = (payload)=>({type: GET_CUSTOMERS,payload})
 
