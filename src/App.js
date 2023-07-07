f2a6cfcacaf9392211de62f1599dec45401df175
Vladimir RM <shvdrummer@gmail.com>
2023-07-07 09:43:16 +0300

    Add  const dispatch  and useSelector

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
 import './App.css';
+import { useDispatch,useSelector } from 'react-redux';
 
 function App() {
+    const dispatch = useDispatch()
+    const cash = useSelector((state)=> state.cash)
   return (
     <div className="App">
+        {cash}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const initialState = {
 }
 
 
-const reducer = (state,action)=>{
+const reducer = (state = initialState,action)=>{
    switch (action.type){
       case  "ADD_CASH":
          return {...state,cash: state.cash + action.payload}
