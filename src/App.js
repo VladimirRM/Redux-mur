630e2ead8c6bfc3eca624d1a33c7b97f6b5246fc
Vladimir RM <shvdrummer@gmail.com>
2023-07-07 09:47:39 +0300

    Check work App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import { useDispatch,useSelector } from 'react-redux';
 function App() {
     const dispatch = useDispatch()
     const cash = useSelector((state)=> state.cash)
+
+
+    const addCash = (cash)=>{
+        dispatch({type:'ADD_CASH', payload: cash})
+    }
+    const getCash = (cash)=>{
+        dispatch({type:'GET_CASH', payload: cash})
+    }
   return (
     <div className="App">
         {cash}
