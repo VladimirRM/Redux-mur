1f73dbef3de81e1c0d8eb638fee286b5f385e2c2
Vladimir RM <shvdrummer@gmail.com>
2023-07-07 09:39:38 +0300

    Create action and reducer

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 
+const reducer = (state,action)=>{
+   switch (action.type){
+      case  "ADD_CASH":
+         return {...state,cash: state.cash + action.payload}
+      case  "GET_CASH":
+         return {...state,cash: state.cash - action.payload}
+
+         default: return state
+   }
+}
+
+
+const store = createStore(reducer)
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
