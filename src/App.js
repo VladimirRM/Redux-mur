833099a4f780fce137a570c0a71f70951098b658
Vladimir RM <shvdrummer@gmail.com>
2023-07-08 08:21:55 +0300

    Create markUp for the render customers

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { useDispatch,useSelector } from 'react-redux';
 function App() {
 
     const dispatch = useDispatch()
-    const cash = useSelector((state)=>state.cash)
+    const cash = useSelector((state)=>state.cash.cash)
+    const customers = useSelector((state)=>state.customers.customers)
 
     const addCash = (cash)=>{
         dispatch({type:'ADD_CASH',payload:cash})
@@ -13,11 +14,25 @@ function App() {
     const getCash = (cash)=>{
         dispatch({type:'GET_CASH',payload:cash})
     }
+
+    const addCustomers = (name)=>{
+        
+    }
   return (
     <div className="App">
         {cash}
         <button onClick={()=>addCash(Number(prompt()))}>Add</button>
         <button onClick={()=>getCash(Number(prompt()))}>Get</button>
+        <button onClick={()=>addCustomers(prompt())}>Add customers</button>
+        <div>
+          {customers.length > 0 ?
+            <div>{customers.map(customer=>
+                <div>{customer.name}</div>
+                )}</div>:
+            <div>Customers not found</div>  
+        }
+
+        </div>
 
     </div>
   );
