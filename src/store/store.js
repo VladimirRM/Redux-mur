744773c904a127043c83a4fe5bcdd1728edd8a0a
Vladimir RM <shvdrummer@gmail.com>
2023-07-08 07:44:05 +0300

    Delete code

---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { combineReducers, createStore } from "redux"
+import {customersReducer} from './customerReducer'
+import {cashReducer}  from './cashReducer'
+
+export const rootReducer = combineReducers({
+   cash: cashReducer,
+   customers: customersReducer,
+})
+
+
+
+
+ export const store = createStore(rootReducer)
