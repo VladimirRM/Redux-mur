679dbe304782a54bb29908618aba71a6ca7a6b77
Vladimir RM <shvdrummer@gmail.com>
2023-07-08 07:45:12 +0300

    Add code template

--- src/App.js
+++ src/App.js
@@ -1,48 +1,9 @@
 
 import './App.css';
-import { useDispatch,useSelector } from 'react-redux';
-import { addCustomersAction } from './store/customerReducer';
-import {removeCustomersAction} from './store/customerReducer1'
 
 function App() {
-    const dispatch = useDispatch()
-    const cash = useSelector((state)=> state.cash.cash)
-    const customers = useSelector((state)=> state.customers.customers)
-
-
-    const addCash = (cash)=>{
-        dispatch({type:'ADD_CASH', payload: cash})
-    }
-    const getCash = (cash)=>{
-        dispatch({type:'GET_CASH', payload: cash})
-    }
-
-    const addCustomers = (name) => {
-        const customer = {
-            name,
-            id:Date.now(),
-        }
-        dispatch(addCustomersAction(customer))
-    }
-
-    const removeCustomer = (customer)=>{
-        dispatch(removeCustomersAction(customer.id))
-    }
   return (
     <div className="App">
-        {cash}
-        <button onClick={()=>addCash(Number(prompt()))}>Add</button>
-        <button onClick={()=>getCash(Number(prompt()))}>Get</button>
-        <button onClick={()=>addCustomers(prompt())}>Get customers</button>
-        <div>
-            {customers.length > 0 ? 
-           <div>{customers.map(customer =>
-            <div onClick={()=> removeCustomer(customer)}
-            >{customer.name}</div>
-            )}</div>:
-           <div>Customers not found</div>    
-        }
-        </div>
     </div>
   );
 }
