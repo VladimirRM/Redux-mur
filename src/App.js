ed11094265497a29847b93f63e4903684df30242
Vladimir RM <shvdrummer@gmail.com>
2023-07-09 08:03:37 +0300

    Add buttons

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
   return (
     <div className="App">
         {cash}
+        <button onClick={()=>addCash(Number(prompt()))}></button>
+        <button onClick={()=>getCash(Number(prompt()))}></button>
     </div>
   );
 }
