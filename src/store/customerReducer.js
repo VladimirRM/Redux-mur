63b1f8c6bacf5a0f7300d3a7e1ba3520e54f3ed5
Vladimir RM <shvdrummer@gmail.com>
2023-07-09 08:14:42 +0300

    create new action in the customerReducer

--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -2,14 +2,18 @@
 const initialState = {
        customers:[],
  }
- 
+const ADD_CUSTOMERS = 'ADD_CUSTOMERS' 
+const REMOVE_CUSTOMERS = 'REMOVE_CUSTOMERS' 
  
   export const customerReducer = (state = initialState,action)=>{
      switch(action.type){
-         case 'ADD_CASH':
+         case ADD_CUSTOMERS:
            return{...state,}
-         case 'GET_CASH':
+         case REMOVE_CUSTOMERS:
            return{...state}
        default:return state
      }
  }
+
+ export const addCustomersAction = (payload)=> ({type: ADD_CUSTOMERS,payload})
+ export const removeCustomersAction = (payload)=> ({type: REMOVE_CUSTOMERS,payload})
