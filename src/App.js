c50284d4465a22383cdbd2488b18763ec3c0ecd1
Vladimir RM <shvdrummer@gmail.com>
2023-07-09 08:16:32 +0300

    add arr customers in App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { useDispatch,useSelector } from 'react-redux';
 function App() {
 
     const dispatch = useDispatch()
-    const cash = useSelector((state)=> state.cash)
+    const cash = useSelector((state)=> state.cash.cash)
+    const customers = useSelector((state)=> state.customers.customers)
 
     const addCash = (cash)=>{
         dispatch({type: "ADD_CASH",payload:cash})
@@ -18,7 +19,12 @@ function App() {
         {cash}
         <button onClick={()=>addCash(Number(prompt()))}>ADD</button>
         <button onClick={()=>getCash(Number(prompt()))}>GET</button>
+        <button onClick={()=>getCash(prompt())}>Add customers</button>
+        <div>
+            {}
+        </div>
     </div>
+    
   );
 }
 
