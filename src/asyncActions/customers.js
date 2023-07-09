4ee66437ac32a9b9f1ba44d79e67f6a2d0abbfed
Vladimir RM <shvdrummer@gmail.com>
2023-07-09 07:52:26 +0300

    Delete All code

--- src/asyncActions/customers.js
+++ src/asyncActions/customers.js
@@ -1,1 +1,9 @@
-export const fetchCustomers = ()
+import { addManyCustomersAction } from "../store/customerReducer"
+
+export const fetchCustomers = ()=>{
+    return  function (dispatch){
+        fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => dispatch(addManyCustomersAction(json)))
+    }
+}
