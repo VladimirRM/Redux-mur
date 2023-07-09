f1cd2c298b6fc7806af7a91d9de2dc3fba34cb04
Vladimir RM <shvdrummer@gmail.com>
2023-07-09 08:00:02 +0300

    Create second action

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ const reducer = (state = initialState,action)=>{
     switch(action.type){
         case 'ADD_CASH':
           return{...state,cash:state.cash + action.payload}
+        case 'GET_CASH':
+          return{...state,cash:state.cash - action.payload}
       default:return state
     }
 }
