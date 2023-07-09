aa4aea47219b8677201cdbc986fad7c4ea9e5cef
Vladimir RM <shvdrummer@gmail.com>
2023-07-09 07:59:40 +0300

    Create func reducer and first action

--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+
+
+const initialState = {
+  cash:0,
+}
+
+
+const reducer = (state = initialState,action)=>{
+    switch(action.type){
+        case 'ADD_CASH':
+          return{...state,cash:state.cash + action.payload}
+      default:return state
+    }
+}
+
 const store = createStore(reducer)
 
 
