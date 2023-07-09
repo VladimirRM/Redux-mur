f41a5666ae5782127a326fdab016d1c92e33199f
Vladimir RM <shvdrummer@gmail.com>
2023-07-09 07:53:58 +0300

    Import createStore

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
