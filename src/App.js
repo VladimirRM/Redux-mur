169c6f737eaea8b8cb51d92b7ffa9ce7f35a5336
Vladimir RM <shvdrummer@gmail.com>
2023-07-10 08:18:42 +0300

    Create obj cash

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 
 import './App.css';
 import { useDispatch,useSelector } from 'react-redux';
-import { addCustomersAction } from './store/customerReducer';
 
 function App() {
-
     const dispatch = useDispatch()
-    const cash = useSelector((state)=> state.cash.cash)
-    const customers = useSelector((state)=> state.customers.customers)
-
-    const addCash = (cash)=>{
-        dispatch({type: "ADD_CASH",payload:cash})
-    }
-    const getCash = (cash)=>{
-        dispatch({type: "GET_CASH",payload:cash})
-    }
-    const addCustomers = ( name)=>{
-        const customer = {
-            name,
-            id:Date.now(),
-        }
-        dispatch(addCustomersAction(customer))
-    }
+    const cash = useSelector((state)=> state.cash)
   return (
     <div className="App">
         {cash}
-        <button onClick={()=>addCash(Number(prompt()))}>ADD</button>
-        <button onClick={()=>getCash(Number(prompt()))}>GET</button>
-        <button onClick={()=>addCustomers(prompt())}>Add customers</button>
-        <div>
-            {}
-        </div>
     </div>
-    
   );
 }
 
