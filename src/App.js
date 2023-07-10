2db2735aab23a7d22902e0717d65df3d6244e86b
Vladimir RM <shvdrummer@gmail.com>
2023-07-10 08:21:49 +0300

    First part  app done.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import { useDispatch,useSelector } from 'react-redux';
 function App() {
     const dispatch = useDispatch()
     const cash = useSelector((state)=> state.cash)
+
+    const addCash = ( cash)=>{
+        dispatch({type:'ADD_CASH',payload:cash})
+    }
+    const getCash = ( cash)=>{
+        dispatch({type:'GET_CASH',payload:cash})
+    }
   return (
     <div className="App">
         {cash}
+        <button onClick={()=>addCash(Number(prompt()))}>Add</button>
+        <button onClick={()=>getCash(Number(prompt()))}>Get</button>
     </div>
   );
 }
