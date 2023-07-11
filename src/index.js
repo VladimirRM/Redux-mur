ae17a180e1cebf10fca88f6258837e625b86f022
Vladimir RM <shvdrummer@gmail.com>
2023-07-11 10:01:51 +0300

    Create actions for arr customers.

--- src/index.js
+++ src/index.js
@@ -2,30 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
-const initialState = {
-  cash: 0,
-}
-export const reducer = (state,action)=>{
-     switch(action.type){
-         case  'ADD_CASH':
-          return {...state,cash: state.cash + action.payload}
-         case  'GET_CASH':
-          return {...state,cash: state.cash - action.payload}
-
-      default: 
-      return state
-     }
-}
+import { Provider } from 'react-redux';
+import {store}  from './store/store'
 
 
 
-export const store = createStore(reducer)
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
+
 );
 
 
---
+++ src/store/customerReducer.js
@@ -0,0 +1,21 @@
+
+const initialState = {
+  customers: [],
+ }
+
+ const ADD_CUSTOMERS = "ADD_CUSTOMERS"
+ const REMOVE_CUSTOMERS = "REMOVE_CUSTOMERS"
+ export const customerReducer = (state = initialState,action)=>{
+      switch(action.type){
+          case  ADD_CUSTOMERS:
+           return {...state,customers: [...state.customers,action.payload]}
+          case  REMOVE_CUSTOMERS:
+           return {...state,customers: state.customers.filter((customer)=> customer.id !==action.payload)}
+ 
+       default: 
+       return state
+      }
+ }
+
+ export const addCustomersAction = (payload)=> ({type:ADD_CUSTOMERS,payload})
+ export const removeCustomersAction = (payload)=> ({type:REMOVE_CUSTOMERS,payload})
