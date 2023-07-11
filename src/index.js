c28f59059c6cb2600ff4181421667bbccdf27788
Vladimir RM <shvdrummer@gmail.com>
2023-07-11 08:45:41 +0300

    Create actions add and get

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ const initialState = {
   cash: 0,
 }
 export const reducer = (state,action)=>{
+     switch(action.type){
+         case  'ADD_CASH':
+          return {...state,cash: state.cash + action.payload}
+         case  'GET_CASH':
+          return {...state,cash: state.cash - action.payload}
 
+      default: 
+      return state
+     }
 }
 
 
