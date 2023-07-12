528188372c58f63b58ad56de8cefb865c5716efb
Vladimir RM <shvdrummer@gmail.com>
2023-07-12 09:11:30 +0300

    Create action for cash

--- src/App.js
+++ src/App.js
@@ -1,43 +1,10 @@
 
 import './App.css';
-import { useDispatch,useSelector } from 'react-redux';
-import { addCustomersAction, removeCustomersAction } from './store/customerReducer';
-import { fetchCustomers } from './asyncActions/customers';
+import
 
 function App() {
-    const dispatch = useDispatch()
-    const cash = useSelector((state)=>state.cash.cash)
-    const customers = useSelector((state)=>state.customers.customers)
-
-    const addCash = (cash) =>{
-        dispatch({type: 'ADD_CASH',payload: cash})
-    }
-    const getCash = (cash) =>{
-        dispatch({type: 'GET_CASH',payload: cash})
-    }
-    const addCustomers = (name)=>{
-        const customer = {
-            name,
-            id:Date.now(),
-        }
-        dispatch(addCustomersAction(customer))
-    }
-    const removeCustomers = (customer)=>{
-        dispatch(removeCustomersAction(customer.id))
-    }
   return (
     <div className="App">
-        {cash}
-        <button onClick={()=>addCash(Number(prompt()))}>Add</button>
-        <button onClick={()=>getCash(Number(prompt()))}>Get</button>
-        <button onClick={()=>addCustomers(prompt())}>Get</button>
-        <button onClick={()=> dispatch(fetchCustomers())}>Add many customers</button>
-        <div>
-           {customers.map((customer)=>
-            <div onClick={()=>removeCustomers(customer)}>{customer.name}</div>
-           )}
-
-        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import {store}  from './store/store'
+import { createStore } from 'redux';
+
+const initialState = {
+  cash: 0,
+}
+
+const reducer = (state,action)=>{
+  switch(action.type){
+    case "ADD_CASH":
+      return {...state,cash: state.cash + action.payload}
+    case "GET_CASH":
+      return {...state,cash: state.cash - action.payload}
+
+    default: return state
+  }
+
+}
 
 
+const store = createStore(reducer)
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </Provider>
+
 
 );
 
