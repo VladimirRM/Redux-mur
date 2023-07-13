619196bcd99e7d0e7dae52faa2f757e724a04ba0
Vladimir RM <shvdrummer@gmail.com>
2023-07-13 17:19:46 +0300

    Add thunk

--- src/index.js
+++ src/index.js
@@ -2,26 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+import {store} from "./store/store"
 import { Provider } from 'react-redux';
 
-const initialState = {
-  cash: 0,
-}
-const reducer = (state = initialState,action)=>{
-  switch(action.type){
-       case "ADD_CASH":
-        return {...state,cash: state.cash + action.payload}
-       case "GET_CASH":
-        return {...state,cash: state.cash - action.payload}
-
-    default:
-      return state
-  }
-}
 
 
-export const store = createStore(reducer)
+
+
 
 
 
