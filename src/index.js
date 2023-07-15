95669ab683685a48220e00b882ec6b995c06c474
Vladimir RM <shvdrummer@gmail.com>
2023-07-15 08:33:10 +0300

    Create main action.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+const reducer = (state,action)=>{
+     switch( action.type ){
+       case "ADD_CASH":
+        return {...state,cash:state.cash + action.payload}
+       case "GET_CASH":
+        return {...state,cash:state.cash - action.payload}
+      default: return state
+     }
+}
+
+const initialState = {
+  cash: 0,
+}
+
 
 const store = createStore(reducer)
 
