222f5f9e597c1d4dc480688972beb5ed396b5b8f
Vladimir RM <shvdrummer@gmail.com>
2023-07-15 08:39:58 +0300

    Import store for index.js

---
+++ src/store/customerReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  customers: [],
+}
+ export const customersReducer = (state = initialState,action)=>{
+     switch( action.type ){
+       case "ADD_CASH":
+        return {...state,cash:state.cash + action.payload}
+       case "GET_CASH":
+        return {...state,cash:state.cash - action.payload}
+      default: return state
+     }
+}
