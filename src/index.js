1d1f14e95d9af1d80d1b6e67521a2bb0c11f6705
Vladimir RM <shvdrummer@gmail.com>
2023-07-15 08:29:09 +0300

    Import and create store.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+
+const store = createStore(reducer)
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  <React.StrictMode>
+<Provider>
+<React.StrictMode>
     <App />
   </React.StrictMode>
+</Provider>
+
 
 
 );
