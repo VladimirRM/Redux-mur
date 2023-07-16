4e858dc80f782c47c96a4053ee29061e41c0a29e
Vladimir RM <shvdrummer@gmail.com>
2023-07-16 09:25:40 +0300

    Create store and add provider.

--- src/index.js
+++ src/index.js
@@ -2,25 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { store } from './store/store';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-
-
-
-
-
-
-
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<Provider store={store}>
-<React.StrictMode>
+    <Provider store={store}>
+  <React.StrictMode>
     <App />
   </React.StrictMode>
-</Provider>
+    </Provider>
 
 
 
