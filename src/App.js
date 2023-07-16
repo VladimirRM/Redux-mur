8a0fb58fc43a7c7b5e53df61ce4d8fdda8132d10
Vladimir RM <shvdrummer@gmail.com>
2023-07-16 10:19:45 +0300

    App done.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 import {useSelector,useDispatch}  from 'react-redux'
+import { addCustomersAction ,  removeCustomersAction} from './store/customerReducer';
+import { fetchCustomers } from './asyncActions/customers';
 
 function App() {
   const dispatch = useDispatch()
-  const cash = useSelector((state)=>state.cash)
+  const cash = useSelector((state)=>state.cash.cash)
+  const customers = useSelector((state)=>state.customers.customers)
 
   const addCash = (cash)=>{
     console.log(cash)
@@ -13,11 +16,29 @@ function App() {
   const getCash = (cash)=>{
     dispatch({type:'GET_CASH',payload:cash})
   }
+  const addCustomers = (name) => {
+    const customer = {
+      name,
+      id:Date.now(),
+    }
+    dispatch(addCustomersAction(customer))
+  }
+  const removeCustomers = (customer)=>{
+    
+    dispatch(removeCustomersAction(customer.id))
+  }
   return (
     <div className="App">
       {cash}
       <button onClick={()=>addCash(Number(prompt()))}>Add</button>
       <button onClick={()=>getCash(Number(prompt()))}>Get</button>
+      <button onClick={()=>addCustomers(prompt())}>Add customers</button>
+      <button onClick={()=>dispatch(fetchCustomers())}>Add customers from data</button>
+      <div>{customers.map((customer)=>(
+        <div key={customer.id}
+        onClick={()=>removeCustomers(customer)}
+        >{customer.name}</div>
+      ))}</div>
     </div>
   );
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,14 @@
-import { createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { cashReducer } from "./cashReducer";
+import {customerReducer} from './customerReducer'
+import thunk from "redux-thunk";
+
+
+export const rootReducer = combineReducers({
+   cash: cashReducer,
+   customers: customerReducer,
+})
 
 
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer,applyMiddleware(thunk))
