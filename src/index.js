f2ad92f0453c5e540fda40473b1642f8159fa66e
Vladimir RM <shvdrummer@gmail.com>
2023-07-16 10:03:12 +0300

    Add rootReducer

--- src/index.js
+++ src/index.js
@@ -4,23 +4,11 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-
-const initialState = {
-  cash:0,
-}
-
-const reducer = (state,action)=>{
-  switch(action,type){
-         case" ADD_CASH":
-         return {...state,cash: state.cash + action.payload}
-         case" GET_CASH":
-         return {...state,cash: state.cash - action.payload}
-    default: return state
-  }
-}
+import {store}  from './store/store'
 
 
-const store = createStore(reducer)
+
+
 
 
 
