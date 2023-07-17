1a660058149a7a3732752b7c48483e78da75cbff
Vladimir RM <shvdrummer@gmail.com>
2023-07-17 08:38:31 +0300

    Create all func.

--- src/index.js
+++ src/index.js
@@ -2,25 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+import {store}  from './store/store'
 import { Provider } from 'react-redux';
 
-const initialState = {
-  cash: 0,
-}
 
-const reducer = (state = initialState,action)=>{
-    switch(action.type){
-        case "ADD_CASH":
-          return {...state,cash:state.cash + action.payload}
-        case "GET_CASH":
-          return {...state,cash:state.cash - action.payload}
 
-      default: return state
-    }
-}
-
-const store  = createStore(reducer)
 
 
 
---
+++ src/store/customerReducer.js
@@ -0,0 +1,23 @@
+const initialState = {
+  customers: [],
+}
+  const ADD_CUSTOMER = 'ADD_CUSTOMER'
+  const ADD_MANY_CUSTOMER = 'ADD_MANY_CUSTOMER'
+  const REMOVE_CUSTOMER = 'REMOVE_CUSTOMER'
+ export const customerReducer = (state = initialState,action)=>{
+    switch(action.type){
+        case "ADD_CUSTOMER":
+          return {...state,customers: [...state.customers, action.payload ]}
+        case "ADD_MANY_CUSTOMER":
+          return {...state,customers: [...state.customers, ...action.payload ]}
+        case "REMOVE_CUSTOMER":
+          return {...state,customers: [...state.customers.filter((customer)=> customer.id !==action.payload)]}
+
+      default: return state
+    }
+}
+
+
+export const addCustomersAction = (payload) => ({type:ADD_CUSTOMER,payload })
+export const addManyCustomersAction = (payload) => ({type:ADD_MANY_CUSTOMER,payload })
+export const removeCustomersAction = (payload) => ({type:REMOVE_CUSTOMER,payload })
