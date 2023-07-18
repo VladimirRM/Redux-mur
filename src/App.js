5177b5e51ccce1766800ee79bb89ad1c9aa72477
Vladimir RM <shvdrummer@gmail.com>
2023-07-18 08:53:25 +0300

    Create all func for add and get cash.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import { useDispatch,useSelector } from 'react-redux';
 function App() {
   const dispatch = useDispatch()
   const cash = useSelector((state)=>state.cash)
+
+  const addCash = (cash)=>{
+    dispatch({type: 'ADD_CASH', payload: cash})
+  }
+  const getCash = (cash)=>{
+    dispatch({type: 'GET_CASH', payload: cash})
+  }
   return (
     <div className="App">
       {cash}
