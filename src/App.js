f7d471754cac8d59e1d9cd913502c9307e4273ca
Vladimir RM <shvdrummer@gmail.com>
2023-07-18 08:51:40 +0300

    Add button.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 
 import './App.css';
+import { useDispatch,useSelector } from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector((state)=>state.cash)
   return (
     <div className="App">
+      {cash}
+      <button onClick={()=> addCash(Number(prompt()))}>Add</button>
+      <button onClick={()=> getCash(Number(prompt()))}>Get</button>
     </div>
   );
 }
