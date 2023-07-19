4edaebd0b82d04e0d80ebf95d7b8aaf5e8cc2838
Vladimir RM <shvdrummer@gmail.com>
2023-07-19 08:30:07 +0300

    Created store and and actions.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,9 @@
 
 import './App.css';
-import { useDispatch,useSelector } from 'react-redux';
-import { addCustomersAction, removeCustomersAction } from './store/customerReducer';
-import {fetchCustomers} from './asyncActions/customers'
 
 function App() {
-  const dispatch = useDispatch()
-  const cash = useSelector((state)=>state.cash.cash)
-  const customers = useSelector((state)=>state.customers.customers)
-
-  const addCash = (cash)=>{
-    dispatch({type: 'ADD_CASH', payload: cash})
-  }
-  const getCash = (cash)=>{
-    dispatch({type: 'GET_CASH', payload: cash})
-  }
-  const addCustomers = (name)=>{
-    const customer = {
-      name,
-      id:Date.now(),
-    }
-    dispatch(addCustomersAction(customer))
-  }
-  const removeCustomer = (customer)=>{
-    dispatch(removeCustomersAction(customer.id))
-  }
   return (
     <div className="App">
-      {cash}
-      <button onClick={()=> addCash(Number(prompt()))}>Add</button>
-      <button onClick={()=> getCash(Number(prompt()))}>Get</button>
-      <button onClick={()=> addCustomers(prompt())}>Add customers</button>
-      <button onClick={()=>dispatch(fetchCustomers()) }>Add customers from data</button>
-      <div >
-      {customers.map((customer)=>(
-        <div key={customer.id}
-        onClick={()=> removeCustomer(customer)}
-        >{customer.name}</div>
-      ))}
-      </div>
     </div>
   );
 }
