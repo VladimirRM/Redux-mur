5dd6feb4af335554814d7e2fbc989adff2803ae3
Vladimir RM <shvdrummer@gmail.com>
2023-07-19 08:31:47 +0300

    Create init cash.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 
 import './App.css';
+import {useSelector,useDispatch} from "react-redux"
 
 function App() {
+
+  const dispatch = useDispatch()
+  const cash = useSelector((state)=> state.cash)
   return (
     <div className="App">
     </div>
