1c1c2194f39c164cf6377c8e952dbca6ec49ad1f
Vladimir RM <shvdrummer@gmail.com>
2023-07-22 08:07:37 +0300

    Add first action.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
+const initialState = {
+    cash:0,
+}
 
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
+export const reducer = (state,action)=>{
+    switch (action.type){
+case'ADD_CASH':
+return {...state,cash: state.cash + action.payload}
 
 
-// );
+
+        default: return state
+    }
+}
+
+const store = createStore(reducer)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<Provider>
+<React.StrictMode>
+    <App />
+  </React.StrictMode>
+</Provider>
+
+
+
+);
 
 
 
