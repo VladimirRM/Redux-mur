d69abe0872aca115ebfcf4a80fefe7498f64db42
Vladimir RM <shvdrummer@gmail.com>
2023-07-22 08:23:53 +0300

    Add actionCreators.

--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -2,11 +2,14 @@ const initialState = {
    customers: [],
 }
 
+const ADD_CUSTOMERS = 'ADD_CUSTOMERS'
+const REMOVE_CUSTOMERS = 'REMOVE_CUSTOMERS'
+
 export const customerReducer = (state = initialState,action)=>{
   switch (action.type){
-case'ADD_CASH':
+case'ADD_CUSTOMERS':
 return {...state,cash: state.cash + action.payload}
-case'GET_CASH':
+case'REMOVE_CASH':
 return {...state,cash: state.cash - action.payload}
 
 
@@ -14,3 +17,5 @@ return {...state,cash: state.cash - action.payload}
       default: return state
   }
 }
+export const addCustomersAction = (payload)=>({ type: ADD_CUSTOMERS,payload})
+export const removeCustomersAction = (payload)=>({ type: REMOVE_CUSTOMERS,payload})
