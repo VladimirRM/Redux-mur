2eee63183187b74ea255205052cd0261d5f1606a
Vladimir RM <shvdrummer@gmail.com>
2023-07-22 08:32:02 +0300

    Add thunk for store.

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import { customerReducer } from "./customerReducer";
 import { cashReducer } from "./cashReducer";
+import thunk from "redux-thunk";
 
 export const rootReducer = combineReducers({
    cash: cashReducer,
    customers: customerReducer
 
 })
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer,applyMiddleware(thunk))
