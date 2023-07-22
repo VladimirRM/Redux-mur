830438b7f4e60d10124c6a9fde5bcddcf89e7855
Vladimir RM <shvdrummer@gmail.com>
2023-07-22 08:31:37 +0300

    Created func addCustomers.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import { useDispatch,useSelector } from 'react-redux';
+import { addCustomersAction } from './store/customerReducer';
 
 function App() {
   const dispatch = useDispatch()
   const cash = useSelector((state)=>state.cash.cash)
+  const customers = useSelector((state)=>state.customers.customers)
 
   const addCash = ( cash)=>{
     dispatch({type: 'ADD_CASH',payload: cash})
@@ -12,11 +14,23 @@ function App() {
   const getCash = ( cash)=>{
     dispatch({type: 'GET_CASH',payload: cash})
   }
+
+  const addCustomers = ( name)=>{
+    const customer = {
+      name,
+      id:Date.now()
+    }
+    dispatch( addCustomersAction(customer))
+  }
   return (
     <div className="App">
       {cash}
       <button onClick={()=>addCash(Number(prompt()))}>Add cash</button>
       <button onClick={()=>getCash(Number(prompt()))}>Get cash</button>
+      <button onClick={()=>addCustomers(prompt())}>Add customers</button>
+      {customers.map(customer=>(
+        <div key={customer.id}>{customer.name}</div>
+      ))}
     </div>
   );
 }
