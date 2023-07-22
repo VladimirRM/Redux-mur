47afca03cc687cdf751b2d09e40f6186fe948fe4
Vladimir RM <shvdrummer@gmail.com>
2023-07-22 08:15:05 +0300

    Created functions add and get Cash.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import { useDispatch,useSelector } from 'react-redux';
 function App() {
   const dispatch = useDispatch()
   const cash = useSelector((state)=>state.cash)
+
+  const addCash = ( cash)=>{
+    dispatch({type: 'ADD_CASH',payload: cash})
+  }
+  const getCash = ( cash)=>{
+    dispatch({type: 'GET_CASH',payload: cash})
+  }
   return (
     <div className="App">
       {cash}
       <button onClick={()=>addCash(Number(prompt()))}>Add cash</button>
-      <button onClick={()=>addCash(Number(prompt()))}>Add cash</button>
+      <button onClick={()=>getCash(Number(prompt()))}>Get cash</button>
     </div>
   );
 }
