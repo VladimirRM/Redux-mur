2025a3079a71746bf7809ce161a3fe5a12686319
Vladimir RM <shvdrummer@gmail.com>
2023-07-27 09:51:28 +0300

    Add dispatch and useSelector.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 
 import './App.css';
+import { useDispatch,useSelector } from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector((state)=>state.cash)
   return (
     <div className="App">
     </div>
