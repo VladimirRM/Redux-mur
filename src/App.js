5b331ab73c92086e3029310441d6d87fea13c80b
Vladimir RM <shvdrummer@gmail.com>
2023-07-27 09:54:21 +0300

    Add const addCash.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import { useDispatch,useSelector } from 'react-redux';
 function App() {
   const dispatch = useDispatch()
   const cash = useSelector((state)=>state.cash)
+  const addCash = (cash)=>{
+    dispatch({type:'ADD_CASH',payload:cash})
+  }
   return (
     <div className="App">
+      {cash}
+      <button onClick={()=>addCash(Number(prompt()))}>Add</button>
+      <button onClick={()=>getCash(Number(prompt()))}>Get</button>
     </div>
   );
 }
