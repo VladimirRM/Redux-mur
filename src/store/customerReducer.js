ca2f24a3ce1d697da6e5f0c6e949475a23264395
Vladimir RM <shvdrummer@gmail.com>
2023-07-28 08:23:20 +0300

    Add new action for the response.

--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -3,10 +3,13 @@ const initialState = {
   customers:[],
 }
 const ADD_CUSTOMER = 'ADD_CUSTOMER'
+const ADD_MANY_CUSTOMER = 'ADD_CUSTOMER'
 const REMOVE_CUSTOMER = 'REMOVE_CUSTOMER'
  export const customersReducer = (state = initialState,action)=>{
   switch(action.type){
     case ADD_CUSTOMER:
+      return {...state,customers: [...state.customers,action.payload]}
+    case ADD_MANY_CUSTOMER:
       return {...state,customers: [...state.customers,action.payload]}
     case REMOVE_CUSTOMER:
       return {...state,cash:state.cash  - action.payload}
@@ -15,4 +18,5 @@ const REMOVE_CUSTOMER = 'REMOVE_CUSTOMER'
   }
 }
 export const addCustomersAction = ( payload)=> ({type:ADD_CUSTOMER,payload})
+export const addManyCustomersAction = ( payload)=> ({type:ADD_MANY_CUSTOMER,payload})
 export const removeCustomersAction = ( payload)=> ({type:REMOVE_CUSTOMER,payload})
