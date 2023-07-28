2e0bb909d00d700c54a1e6b1f1115bf02d85e5e4
Vladimir RM <shvdrummer@gmail.com>
2023-07-28 08:20:46 +0300

    Add thunk for get response.

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import { cashReducer } from "./cashReducer";
 import {customerReducer} from './customerReducer1'
+import thunk from "redux-thunk";
 
 export const rootReducer = combineReducers({
    cash: cashReducer,
    customers: customerReducer
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer,applyMiddleware(thunk))
